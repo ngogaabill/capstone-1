304c114cfaba1b4e19def30b1ff59dc475e950d4
Billngoga <ngogaabill@example.com>
2025-10-11 22:23:11 -0500

    Added multiple methods,saved data to file and to respective Lists and print.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,196 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Transactions> debitTransactions = new ArrayList<>();//hold the deposits +
+    static ArrayList<Transactions> paymentTransactions = new ArrayList<>();//hold the payments
+    static String fileName = "src/main/resources/transactions.csv";
+
+    public static void main(String[] args) {
+        System.out.println("==========WELCOME TO LEDGER APPLICATION==========");
+        homepage();
+    }
+
+    /**
+     * Homepage() gucci
+     *
+     */
+
+    private static void homepage() {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("D) Add Deposit\n" +
+                    "P) Make Payment (Debit)\n" +
+                    "L) Ledger\n" +
+                    "E) Exit\n" +
+                    "Your Choice:");
+            String input = scanner.nextLine();
+            char userChoice = input.charAt(0);
+            switch (userChoice) {
+                case 'D':
+                    addDeposit();
+                    break;
+                case 'P':
+                    makePayment();
+                    break;
+                case 'L':
+                    ledger();
+                    break;
+                case 'E':
+                    exit = true;
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Adds deposits to the arrayList
+     *
+     */
+    private static void addDeposit() {
+        getInfoFromUser(debitTransactions, false);
+
+    }
+
+    /**
+     * Add Payments to the list
+     */
+    private static void makePayment() {
+        getInfoFromUser(paymentTransactions, true);
+    }
+    /**
+     * ledger Manuel
+     */
+    private static void ledger() {
+        boolean returnHome = false;
+
+        while (!returnHome) {
+            System.out.println("A)Display all entries\n" +
+                    "D) Deposits\n" +
+                    "account\n" +
+                    "P) Payments\n" +
+                    "R) Reports\n" +
+                    "H) Home\n" +
+                    "Your Choice:");
+            String input = scanner.nextLine();
+            char userChoice = input.charAt(0);
+            switch (userChoice) {
+                case 'A':
+                    allEntries();
+                    break;
+                case 'D':
+                    deposits();
+                    break;
+                case 'P':
+                    payments();
+                    break;
+                case 'R':
+                    report();
+                    break;
+                case 'H':
+                    returnHome = true;
+
+            }
+        }
+
+    }
+
+    private static void report() {
+
+    }
+
+    /**
+     * Print Debits in the debit List
+     */
+    private static void deposits() {
+        System.out.println("Your Deposit List:");
+        for (int i = 0; i < debitTransactions.size(); i++) {
+            System.out.println(debitTransactions.get(i).toString());
+        }
+    }
+
+    /**
+     * Print payments in the payment List
+     */
+    private static void payments() {
+        System.out.println("Your Payment List:");
+        for (int i = 0; i < paymentTransactions.size(); i++) {
+            System.out.println(paymentTransactions.get(i).toString());
+        }
+    }
+
+    /**
+     * Print all data reading them from the file
+     */
+    private static void allEntries() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String entries;
+            while ((entries = bufferedReader.readLine()) != null) {
+                System.out.println(entries);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * Get Info from user based on the transactions save to respective ArrayLists
+     * @param transactionsType
+     * @param isPayment
+     */
+    public static void getInfoFromUser(ArrayList<Transactions> transactionsType, boolean isPayment) {
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
+        String timeDateStamp = currentTime.format(formatter);
+        String[] timeDateParts = timeDateStamp.split("\\|");
+        String date = timeDateParts[0];
+        String time = timeDateParts[1];
+        System.out.println("Enter payment Description:");
+        String description = scanner.nextLine();
+        System.out.println("Vendor:");
+        String vendor = scanner.nextLine();
+        System.out.println("Amount:");
+        double amount = scanner.nextDouble();
+
+        if (isPayment) { //if so turn into neg amount
+            amount *= -1;
+        }
+
+        scanner.nextLine();//consume the newline
+        Transactions transactions = new Transactions(date, time, description, vendor, amount);
+        transactionsType.add(transactions);
+
+        File file = new File(fileName);
+        boolean noHeader = file.length() == 0;
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName, true))) {
+            if (noHeader) {
+                String header = "date|time|description|vendor|amount";
+                bufferedWriter.write(header);
+                bufferedWriter.newLine();
+            }
+
+            String dataToFile = date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+            bufferedWriter.write(dataToFile);
+            bufferedWriter.newLine(); // Add a new line after each transaction
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
+
+
+
+
--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,5 +1,63 @@
 package com.pluralsight;
 
-public class Reports {
+public class Transactions {
+    //date|time|description|vendor|amount
+    private String vendor;
+    private double amount;
+    private  String description;
+    private String time;
+    private String date;
+
+    public Transactions(String date, String time, String description, String vendor,double amount) {
+        this.vendor = vendor;
+        this.time = time;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String toString(){
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+
 
 }
